9cec06c4bab58f743eaf145ff77918ad4516904e
Awarduuu <award_uuu@naver.com>
2023-03-21 09:52:29 +0900

    B9663_NQueen_윤상우

---
+++ src/Day8/B9663_NQueen/B9663_NQueen_윤상우.java
@@ -0,0 +1,58 @@
+package Day8.B9663_NQueen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B9663_NQueen_윤상우 {
+	
+	static int n;
+	static int [] arr;
+ 	static boolean [] isSelected;
+	static int cnt;
+	
+	static boolean checkValid(int x, int y) {
+		for(int i=0; i<n; i++) {
+			if(y == i || arr[i] == -1) continue;						// 자기자신이나 안 고른 경우는 pass
+			if(Math.abs(y - i) == Math.abs(x - arr[i])) return false;	// 대각선 걸리는 경우
+		}
+		return true;
+	}
+	
+	static void queen(int r) {
+		if(r>=n) {
+			cnt++;
+			return;
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(isSelected[i]) continue;
+			if(!checkValid(i, r)) continue;
+			
+			isSelected[i] = true;
+			arr[r] = i;
+			
+			queen(r+1);
+			
+			isSelected[i] = false;
+			arr[r] = -1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		arr = new int [n];
+		isSelected = new boolean [n];
+		cnt = 0;
+		
+		for(int i=0; i<n; i++) arr[i] = -1;
+		queen(0);
+		
+		
+		System.out.println(cnt);
+		
+		
+	}
+
+}
